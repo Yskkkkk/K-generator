e405dbbe661a98b12d88f3ef9ddf2fd7e63d831d
Yskkkkk <1712320578@qq.com>
2024-01-13 19:19:21 +0800

    制作工具配置能力增强

--- k-generator-maker/src/main/java/com/ysk/maker/generator/main/GenerateTemplate.java
+++ k-generator-maker/src/main/java/com/ysk/maker/generator/main/GenerateTemplate.java
@@ -33,8 +33,10 @@ public abstract class GenerateTemplate {
         // 5、生成精简版的程序（产物包）
         buildDist(outputPath, sourceCopyDestPath, jarPath, shellOutputFilePath);
     }
+
     /**
      * 生成精简版程序
+     *
      * @param outputPath
      * @param sourceCopyDestPath
      * @param jarPath
@@ -52,6 +54,7 @@ public abstract class GenerateTemplate {
         // 拷贝源模板文件
         FileUtil.copy(sourceCopyDestPath, distOutputPath, true);
     }
+
     /**
      * 封装脚本
      *
@@ -65,8 +68,10 @@ public abstract class GenerateTemplate {
         ScriptGenerator.doGenerate(shellOutputFilePath, jarPath);
         return shellOutputFilePath;
     }
+
     /**
      * 构建 jar 包
+     *
      * @param outputPath
      * @return 返回 jar 包的相对路径
      * @throws IOException
@@ -78,64 +83,69 @@ public abstract class GenerateTemplate {
         String jarPath = "target/" + jarName;
         return jarPath;
     }
+
     /**
      * 代码生成
+     *
      * @param meta
      * @param outputPath
-    * @throws IOException
-    * @throws TemplateException
-    */
-protected void generateCode(Meta meta, String outputPath) throws IOException, TemplateException {
-    // 读取 resources 目录
-    ClassPathResource classPathResource = new ClassPathResource("");
-    String inputResourcePath = classPathResource.getAbsolutePath();
-    // Java 包基础路径
-    String outputBasePackage = meta.getBasePackage();
-    String outputBasePackagePath = StrUtil.join("/", StrUtil.split(outputBasePackage, "."));
-    String outputBaseJavaPackagePath = outputPath + File.separator + "src/main/java/" + outputBasePackagePath;
-    String inputFilePath;
-    String outputFilePath;
-    // model.DataModel
-    inputFilePath = inputResourcePath + File.separator + "templates/java/model/DataModel.java.ftl";
-    outputFilePath = outputBaseJavaPackagePath + "/model/DataModel.java";
-    DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
-    // cli.command.ConfigCommand
-    inputFilePath = inputResourcePath + File.separator + "templates/java/cli/command/ConfigCommand.java.ftl";
-    outputFilePath = outputBaseJavaPackagePath + "/cli/command/ConfigCommand.java";
-    DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
-    // cli.command.GenerateCommand
-    inputFilePath = inputResourcePath + File.separator + "templates/java/cli/command/GenerateCommand.java.ftl";
-    outputFilePath = outputBaseJavaPackagePath + "/cli/command/GenerateCommand.java";
-    DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
-    // cli.command.ListCommand
-    inputFilePath = inputResourcePath + File.separator + "templates/java/cli/command/ListCommand.java.ftl";
-    outputFilePath = outputBaseJavaPackagePath + "/cli/command/ListCommand.java";
-    DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
-    // cli.CommandExecutor
-    inputFilePath = inputResourcePath + File.separator + "templates/java/cli/CommandExecutor.java.ftl";
-    outputFilePath = outputBaseJavaPackagePath + "/cli/CommandExecutor.java";
-    DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
-    // Main
-    inputFilePath = inputResourcePath + File.separator + "templates/java/Main.java.ftl";
-    outputFilePath = outputBaseJavaPackagePath + "/Main.java";
-    DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
-    // generator.DynamicGenerator
-    inputFilePath = inputResourcePath + File.separator + "templates/java/generator/DynamicGenerator.java.ftl";
-    outputFilePath = outputBaseJavaPackagePath + "/generator/DynamicGenerator.java";
-    DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
-    // generator.MainGenerator
-    inputFilePath = inputResourcePath + File.separator + "templates/java/generator/MainGenerator.java.ftl";
-    outputFilePath = outputBaseJavaPackagePath + "/generator/MainGenerator.java";
-    DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
-    // generator.StaticGenerator
-    inputFilePath = inputResourcePath + File.separator + "templates/java/generator/StaticGenerator.java.ftl";
-    outputFilePath = outputBaseJavaPackagePath + "/generator/StaticGenerator.java";
-    DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
-    // pom.xml
-    inputFilePath = inputResourcePath + File.separator + "templates/pom.xml.ftl";
-    outputFilePath = outputPath + File.separator + "pom.xml";
-    DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
-}
+     * @throws IOException
+     * @throws TemplateException
+     */
+    protected void generateCode(Meta meta, String outputPath) throws IOException, TemplateException {
+        // 读取 resources 目录
+        ClassPathResource classPathResource = new ClassPathResource("");
+        String inputResourcePath = classPathResource.getAbsolutePath();
+        // Java 包基础路径
+        String outputBasePackage = meta.getBasePackage();
+        String outputBasePackagePath = StrUtil.join("/", StrUtil.split(outputBasePackage, "."));
+        String outputBaseJavaPackagePath = outputPath + File.separator + "src/main/java/" + outputBasePackagePath;
+        String inputFilePath;
+        String outputFilePath;
+        // model.DataModel
+        inputFilePath = inputResourcePath + File.separator + "templates/java/model/DataModel.java.ftl";
+        outputFilePath = outputBaseJavaPackagePath + "/model/DataModel.java";
+        DynamicFileGenerator.doGenerate(inputFilePath, outputFilePath, meta);
+
+        // generator.MainGenerator
+        inputFilePath = inputResourcePath + File.separator + "templates/java/generator/MainGenerator.java.ftl";
+        outputFilePath = outputBaseJavaPackagePath + "/generator/MainGenerator.java";
+        DynamicFileGenerator.doGenerate(inputFilePath, outputFilePath, meta);
+        // cli.command.ConfigCommand
+        inputFilePath = inputResourcePath + File.separator + "templates/java/cli/command/ConfigCommand.java.ftl";
+        outputFilePath = outputBaseJavaPackagePath + "/cli/command/ConfigCommand.java";
+        DynamicFileGenerator.doGenerate(inputFilePath, outputFilePath, meta);
+        // cli.command.GenerateCommand
+        inputFilePath = inputResourcePath + File.separator + "templates/java/cli/command/GenerateCommand.java.ftl";
+        outputFilePath = outputBaseJavaPackagePath + "/cli/command/GenerateCommand.java";
+        DynamicFileGenerator.doGenerate(inputFilePath, outputFilePath, meta);
+        // cli.command.ListCommand
+        inputFilePath = inputResourcePath + File.separator + "templates/java/cli/command/ListCommand.java.ftl";
+        outputFilePath = outputBaseJavaPackagePath + "/cli/command/ListCommand.java";
+        DynamicFileGenerator.doGenerate(inputFilePath, outputFilePath, meta);
+        // cli.CommandExecutor
+        inputFilePath = inputResourcePath + File.separator + "templates/java/cli/CommandExecutor.java.ftl";
+        outputFilePath = outputBaseJavaPackagePath + "/cli/CommandExecutor.java";
+        DynamicFileGenerator.doGenerate(inputFilePath, outputFilePath, meta);
+        // Main
+        inputFilePath = inputResourcePath + File.separator + "templates/java/Main.java.ftl";
+        outputFilePath = outputBaseJavaPackagePath + "/Main.java";
+        DynamicFileGenerator.doGenerate(inputFilePath, outputFilePath, meta);
+        // generator.DynamicGenerator
+        inputFilePath = inputResourcePath + File.separator + "templates/java/generator/DynamicGenerator.java.ftl";
+        outputFilePath = outputBaseJavaPackagePath + "/generator/DynamicGenerator.java";
+        DynamicFileGenerator.doGenerate(inputFilePath, outputFilePath, meta);
+
+        // generator.StaticGenerator
+        inputFilePath = inputResourcePath + File.separator + "templates/java/generator/StaticGenerator.java.ftl";
+        outputFilePath = outputBaseJavaPackagePath + "/generator/StaticGenerator.java";
+        DynamicFileGenerator.doGenerate(inputFilePath, outputFilePath, meta);
+        // pom.xml
+        inputFilePath = inputResourcePath + File.separator + "templates/pom.xml.ftl";
+        outputFilePath = outputPath + File.separator + "pom.xml";
+        DynamicFileGenerator.doGenerate(inputFilePath, outputFilePath, meta);
+    }
+
     /**
      * 复制原始文件
      *
--- k-generator-maker/src/main/java/com/ysk/maker/meta/MetaValidator.java
+++ k-generator-maker/src/main/java/com/ysk/maker/meta/MetaValidator.java
@@ -10,6 +10,7 @@ import com.ysk.maker.meta.enums.ModelTypeEnum;
 
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 元信息校验
@@ -31,6 +32,17 @@ public class MetaValidator {
             return;
         }
         for (Meta.ModelConfig.ModelInfo modelInfo : modelInfoList) {
+            // 为group，不校验
+            String groupKey = modelInfo.getGroupKey();
+            if(StrUtil.isNotEmpty(groupKey)){
+                // 生成中间参数
+                List<Meta.ModelConfig.ModelInfo> subModelInfoList = modelInfo.getModels();
+                String allArgStr = modelInfo.getModels().stream()
+                        .map(subModelInfo -> String.format("\"--%s\"", subModelInfo.getFieldName()))
+                        .collect(Collectors.joining(","));
+                modelInfo.setAllArgsStr(allArgStr);
+                continue;
+            }
             // 输出路径默认值
             String fieldName = modelInfo.getFieldName();
             if (StrUtil.isBlank(fieldName)) {
@@ -76,6 +88,11 @@ public class MetaValidator {
             return;
         }
         for (Meta.FileConfig.FileInfo fileInfo : fileInfoList) {
+            String type = fileInfo.getType();
+            // 类型为group，不校验
+            if(FileTypeEnum.GROUP.getValue().equals(type)){
+                continue;
+            }
             // inputPath: 必填
             String inputPath = fileInfo.getInputPath();
             if (StrUtil.isBlank(inputPath)) {
@@ -88,7 +105,6 @@ public class MetaValidator {
                 fileInfo.setOutputPath(inputPath);
             }
             // type：默认 inputPath 有文件后缀（如 .java）为 file，否则为 dir
-            String type = fileInfo.getType();
             if (StrUtil.isBlank(type)) {
                 // 无文件后缀
                 if (StrUtil.isBlank(FileUtil.getSuffix(inputPath))) {
